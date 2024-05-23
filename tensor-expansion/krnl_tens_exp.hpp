541cd28d65a8bea76289569fec8454f9e6872c78
angelozangari <angelo.zangari10@gmail.com>
2024-05-23 23:23:14 +0200

    add header kernel

---
+++ tensor-expansion/krnl_tens_exp.hpp
@@ -0,0 +1,12 @@
+/*
+ * insert out licence 
+ */
+
+#ifndef _KRNL_TE_H_
+#define _KRNL_TE_H_
+
+extern "C" {
+    void krnl_te(uint32_t* A, uint32_t* B, uint32_t* C, int row_B, int col_B, int size_A);
+}
+
+#endif
