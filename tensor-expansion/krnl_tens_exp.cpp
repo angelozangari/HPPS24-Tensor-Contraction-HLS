5e5123fd816c3531b064ea8fa10b6efd1dd7346a
angelozangari <angelo.zangari10@gmail.com>
2024-05-23 23:35:23 +0200

    add impl tens exp in kernel code

--- tensor-expansion/krnl_tens_exp.cpp
+++ tensor-expansion/krnl_tens_exp.cpp
@@ -1,5 +1,25 @@
-// Pseudocode for tensor expansion kernel
+/*
+ * insert out licence 
+ */
 
+// Pseudocode for tensor expansion kernel
 // C = A x B, where x is tensor expansion symbol
 
-void krnl_te(uint32_t* A, uint32_t* B, uint32_t* C, int size_B, int size_A);
+#include "krnl_tens_exp.hpp"
+
+void krnl_te(
+    uint32_t* A,
+    uint32_t* B,
+    uint32_t* C,
+    int row_B,
+    int col_B,
+    int size_A
+) {
+    for (int i = 0; i < size_A; i++) {
+        for (int j = 0; j < row_B * col_B; j++) {
+            #pragma HLS PIPELINE II=1
+            // TODO: fix index logic
+            C[i * row_B * col_B + j] = A[i] * B[j];
+        }
+    }
+}
