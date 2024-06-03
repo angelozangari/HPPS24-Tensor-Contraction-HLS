ea1d20fc325d9fd4c2ff02cc53d5c0d5f602bca5
angelozangari <angelo.zangari10@gmail.com>
2024-06-04 00:56:26 +0200

    update impl matmul header

--- matrix-multiplication/krnl_mat_mul.h
+++ matrix-multiplication/krnl_mat_mul.h
@@ -1,15 +1,38 @@
 #pragma once
 
+#include <cstdint>
+
+#include "../types.h"
+#include "ap_int.h"
+#include "hls_stream.h"
+
 extern "C" {
-    void matrix_multiplication();
+/**
+ * @brief Kernel for matrix multiplication
+ */
+void matrix_multiplication(coo_t *A, coo_t *B, coo_t *C, dim_t A_size, dim_t B_size,
+                            dim_t C_size);
 }
 
 namespace matrix {
 
-void load();
+/** 
+ * @brief Load a COO tensor from memory to a stream
+*/
+void load(coo_t *A, hls::stream<coo_t> &A_stream, dim_t A_size);
 
-void store();
+/**
+ * @brief Store a COO tensor from a stream to memory
+ */
+void store(coo_t *C, hls::stream<coo_t> &C_stream, dim_t C_size);
 
-void compute();
+namespace multiplication {
 
+/**
+ * @brief Compute the matrix multiplication C = A * B
+ */
+void compute(hls::stream<coo_t> &A_stream, hls::stream<coo_t> &B_stream,
+             hls::stream<coo_t> &C_stream, dim_t A_size, dim_t B_size);
+
+} // namespace multiplication
 } // namespace matrix
