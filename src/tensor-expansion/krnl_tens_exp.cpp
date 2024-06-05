7c4b781995f820d55e6a5ed1662f3c9674e23fd9
Federico Lolli <federico123579@gmail.com>
2024-06-05 18:08:32 +0200

    Commented unfinished code

--- src/tensor-expansion/krnl_tens_exp.cpp
+++ src/tensor-expansion/krnl_tens_exp.cpp
@@ -44,45 +44,46 @@ namespace expansion {
 
 void compute(hls::stream<coo_t> &A_stream, hls::stream<coo_t> &B_stream,
              hls::stream<coo_t> &C_stream, rank_t A_R, rank_t B_R) {
-  hls::stream<coo_t> aij, blm;
-  coo_t a1, a2, b1, b2, c;
-  dim_t i = 0, j = 0;
+  // FIXME: still implementing the compute function
+  // hls::stream<coo_t> aij, blm;
+  // coo_t a1, a2, b1, b2, c;
+  // dim_t i = 0, j = 0;
 
-  a1 = A_stream.read();
-  aij.write(a1);
-  i++;
-  a2 = A_stream.read();
-  while (a1.y == a2.y) {
-    aij.write(a2);
-    i++;
-    a2 = A_stream.read();
-  }
+  // a1 = A_stream.read();
+  // aij.write(a1);
+  // i++;
+  // a2 = A_stream.read();
+  // while (a1.y == a2.y) {
+  //   aij.write(a2);
+  //   i++;
+  //   a2 = A_stream.read();
+  // }
 
-  b1 = B_stream.read();
-  blm.write(b1);
-  j++;
-  b2 = B_stream.read();
-  while (b1.y == b2.y) {
-    blm.write(b2);
-    j++;
-    b2 = B_stream.read();
-  }
+  // b1 = B_stream.read();
+  // blm.write(b1);
+  // j++;
+  // b2 = B_stream.read();
+  // while (b1.y == b2.y) {
+  //   blm.write(b2);
+  //   j++;
+  //   b2 = B_stream.read();
+  // }
 
-  for (int k = 0; k < i; k++) {
-    a1 = aij.read();
-    for (int l = 0; l < j; l++) {
-      b1 = blm.read();
-      c.data = Complex::mul(a1.data, b1.data);
-      c.x = a1.x * A_R + b1.x;
-      c.y = a1.y * A_R + b1.y;
-      C_stream.write(c);
-    }
-  }
+  // for (int k = 0; k < i; k++) {
+  //   a1 = aij.read();
+  //   for (int l = 0; l < j; l++) {
+  //     b1 = blm.read();
+  //     c.data = Complex::mul(a1.data, b1.data);
+  //     c.x = a1.x * A_R + b1.x;
+  //     c.y = a1.y * A_R + b1.y;
+  //     C_stream.write(c);
+  //   }
+  // }
 
-  a2 = A_stream.read();
-  while (a1.y == a2.y) {
-    aij.write(a2);
-  }
+  // a2 = A_stream.read();
+  // while (a1.y == a2.y) {
+  //   aij.write(a2);
+  // }
 }
 
 } // namespace expansion
