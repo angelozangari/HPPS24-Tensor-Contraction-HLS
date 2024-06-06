aec6d8af065071f19cf16b40857b106212d8beb7
angelozangari <angelo.zangari10@gmail.com>
2024-06-06 18:02:38 +0200

    fix namespace for consistency

--- src/matrix-multiplication/krnl_mat_mul.h
+++ src/matrix-multiplication/krnl_mat_mul.h
@@ -14,9 +14,7 @@ void matrix_multiplication(coo_t *A, coo_t *B, coo_t *C, dim_t A_size,
                            dim_t B_size, dim_t C_size);
 }
 
-namespace matrix {
-
-namespace multiplication {
+namespace Matrix::Multiplication {
 
 /**
  * @brief Compute the matrix multiplication C = A * B
@@ -24,5 +22,4 @@ namespace multiplication {
 void compute(hls::stream<coo_t> &A_stream, hls::stream<coo_t> &B_stream,
              hls::stream<coo_t> &C_stream, dim_t A_size, dim_t B_size);
 
-} // namespace multiplication
-} // namespace matrix
+} // namespace Matrix::Multiplication
