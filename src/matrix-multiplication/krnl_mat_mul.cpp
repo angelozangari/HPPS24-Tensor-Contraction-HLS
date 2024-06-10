43aea8871068088e8f528f5403d208b43cc9bf6b
angelozangari <angelo.zangari10@gmail.com>
2024-06-10 10:06:16 +0200

    add load/store impl

--- src/matrix-multiplication/krnl_mat_mul.cpp
+++ src/matrix-multiplication/krnl_mat_mul.cpp
@@ -2,9 +2,32 @@
 
 using namespace Complex;
 
-//void matrix_multiplication() {}
+void matrix_multiplication(coo_t *A, coo_t *B, coo_t *C) {
+
+
+}
 
 namespace matrix {
+
+void load(coo_t *A, hls::stream<coo_t> &A_stream, dim_t A_size) {
+  for (int i = 0; i < A_size; i++) {
+    // clang-format off
+#pragma HLS PIPELINE II=1
+    // clang-format on
+    A_stream.write(A[i]);
+  }
+}
+
+void store(hls::stream<coo_t> &C_stream, coo_t *C, dim_t C_size) {
+  for (int i = 0; i < C_size; i++) {
+    // clang-format off
+#pragma HLS PIPELINE II=1
+    // clang-format on
+    C[i] = C_stream.read();
+  }
+}
+
+namespace Multiplication {
 
 void compute();
 
@@ -16,4 +39,5 @@ void compute();
 
 // reiterate on next row of A
 
+} // namespace Multiplication
 } // namespace matrix
