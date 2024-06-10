b414946a69984c1af0e0a75491e4c349f70d3180
angelozangari <angelo.zangari10@gmail.com>
2024-06-10 09:43:21 +0200

    added structure for 2x2 matrix multiplication in coo

--- src/matrix-multiplication/krnl_mat_mul.cpp
+++ src/matrix-multiplication/krnl_mat_mul.cpp
@@ -6,4 +6,12 @@ namespace matrix {
 
 void compute();
 
+// allocate 2 streams to store the two columnns of B
+
+// allocate 1 stream to store a column of A
+
+// FOR loop to compute 1 row of C at a time
+
+// reiterate on next row of A
+
 } // namespace matrix
