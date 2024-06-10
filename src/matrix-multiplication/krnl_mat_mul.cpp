d3a187379daecb053eb4e67e165671bf8ccabfb9
angelozangari <angelo.zangari10@gmail.com>
2024-06-10 14:33:47 +0200

    remove load-store from matrix

--- src/matrix-multiplication/krnl_mat_mul.cpp
+++ src/matrix-multiplication/krnl_mat_mul.cpp
@@ -30,26 +30,6 @@ void matrix_multiplication(coo_t *A, coo_t *B, coo_t *C,
 
 namespace matrix {
 
-/*
-void load(coo_t *A, hls::stream<coo_t> &A_stream, dim_t A_size) {
-  for (int i = 0; i < A_size; i++) {
-    // clang-format off
-#pragma HLS PIPELINE II=1
-    // clang-format on
-    A_stream.write(A[i]);
-  }
-}
-
-void store(hls::stream<coo_t> &C_stream, coo_t *C, dim_t C_size) {
-  for (int i = 0; i < C_size; i++) {
-    // clang-format off
-#pragma HLS PIPELINE II=1
-    // clang-format on
-    C[i] = C_stream.read();
-  }
-}
-*/
-
 namespace Multiplication {
 
 void compute(hls::stream<coo_t> &A_stream, hls::stream<coo_t> &B_stream,
