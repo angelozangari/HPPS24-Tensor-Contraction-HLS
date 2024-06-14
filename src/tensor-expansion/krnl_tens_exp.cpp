d7ab83ed6a054330824cfd30c1f7d5462f11f5c9
Federico Lolli <federico123579@gmail.com>
2024-06-14 18:51:08 +0200

    wip

--- src/tensor-expansion/krnl_tens_exp.cpp
+++ src/tensor-expansion/krnl_tens_exp.cpp
@@ -6,9 +6,9 @@ using namespace Complex;
 void tensor_expansion(coo_t *A, coo_t *B, coo_t *C, dim_t A_NZ, dim_t B_NZ,
                       rank_t A_R, rank_t B_R) {
   // clang-format off
-#pragma HLS INTERFACE m_axi port=A bundle=gmem0 //depth=1024
-#pragma HLS INTERFACE m_axi port=B bundle=gmem1 //depth=1024
-#pragma HLS INTERFACE m_axi port=C bundle=gmem2 //depth=1024
+#pragma HLS INTERFACE m_axi port=A bundle=gmem0 depth=2
+#pragma HLS INTERFACE m_axi port=B bundle=gmem1 depth=4
+#pragma HLS INTERFACE m_axi port=C bundle=gmem2 depth=8
 #pragma HLS INTERFACE s_axilite port=A bundle=control
 #pragma HLS INTERFACE s_axilite port=B bundle=control
 #pragma HLS INTERFACE s_axilite port=C bundle=control
@@ -20,6 +20,11 @@ void tensor_expansion(coo_t *A, coo_t *B, coo_t *C, dim_t A_NZ, dim_t B_NZ,
   // clang-format on
 
   hls::stream<coo_t> A_stream, B_stream, C_stream;
+  // clang-format off
+#pragma HLS STREAM variable=A_stream depth=2
+#pragma HLS STREAM variable=A_stream depth=4
+#pragma HLS STREAM variable=A_stream depth=8
+  // clang-format on
 
 #pragma HLS dataflow
   Tensor::load(A, A_stream, A_NZ);
--- src/tests/tensor-expansion/test_tensor_expansion.cpp
+++ src/tests/tensor-expansion/test_tensor_expansion.cpp
@@ -21,51 +21,54 @@ int main() {
   reader.consume();
   auto ops = &reader.operations;
 
-  for (size_t i = 0; i < ops->size(); i++) {
-    OP &op = ops->at(i);
+  // for (size_t i = 0; i < ops->size(); i++) {
+  int i = 0;
+  OP &op = ops->at(i);
 
-    CooTens left{op.left};
-    CooTens right{op.right};
-    CooTens real_out{op.out};
+  CooTens left{op.left};
+  CooTens right{op.right};
+  CooTens real_out{op.out};
 
-    // Call the kernel
-    std::vector<coo_t> out(left.size() * right.size());
-    cout << "Running test " << i << " with sizes " << left.rank << " x "
-         << right.rank << " -> " << real_out.rank << " ... " << flush;
-    tensor_expansion(left.data.data(), right.data.data(), out.data(),
-                     left.size(), right.size(), left.rank, right.rank);
+  // Call the kernel
+  std::vector<coo_t> out(left.size() * right.size());
+  cout << "Running test " << i << " with sizes " << left.rank << " x "
+       << right.rank << " -> " << real_out.rank << " ... " << flush;
+  tensor_expansion(left.data.data(), right.data.data(), out.data(), left.size(),
+                   right.size(), left.rank, right.rank);
 
-    // Compare the output
-    CooTens predicted_out{out, left.rank * 2};
+  // Compare the output
+  CooTens predicted_out{out, left.rank * 2};
 
-    if (predicted_out.data.size() != real_out.data.size()) {
+  if (predicted_out.data.size() != real_out.data.size()) {
+    cout << "FAILED" << endl;
+    cout << "Mismatch in sizes" << endl;
+    cout << "Predicted output size: " << predicted_out.data.size() << endl;
+    cout << "Real output size: " << real_out.data.size() << endl;
+    return 1;
+  }
+
+  for (size_t i = 0; i < predicted_out.data.size(); i++) {
+    if (!(predicted_out.data[i] == real_out.data[i])) {
       cout << "FAILED" << endl;
-      cout << "Mismatch in sizes" << endl;
-      cout << "Predicted output size: " << predicted_out.data.size() << endl;
-      cout << "Real output size: " << real_out.data.size() << endl;
+      cout << "Mismatch in data" << endl;
+      // print_op_matrices(op);
+      cout << "Predicted output:"
+           << "(" << predicted_out.data[i].data.real << " + "
+           << predicted_out.data[i].data.imag << "i) at ("
+           << predicted_out.data[i].x << ", " << predicted_out.data[i].y << ")"
+           << endl;
+      cout << "Real output:"
+           << "(" << real_out.data[i].data.real << " + "
+           << real_out.data[i].data.imag << "i) at (" << real_out.data[i].x
+           << ", " << real_out.data[i].y << ")" << endl;
+      cout << "Full Real output:" << endl;
+      real_out.print();
+      cout << "Full Predicted output:" << endl;
+      predicted_out.print();
+      op.print();
       return 1;
     }
-
-    for (size_t i = 0; i < predicted_out.data.size(); i++) {
-      if (!(predicted_out.data[i] == real_out.data[i])) {
-        cout << "FAILED" << endl;
-        cout << "Mismatch in data" << endl;
-        // print_op_matrices(op);
-        cout << "Predicted output:" << "(" << predicted_out.data[i].data.real
-             << " + " << predicted_out.data[i].data.imag << "i) at ("
-             << predicted_out.data[i].x << ", " << predicted_out.data[i].y
-             << ")" << endl;
-        cout << "Real output:" << "(" << real_out.data[i].data.real << " + "
-             << real_out.data[i].data.imag << "i) at (" << real_out.data[i].x
-             << ", " << real_out.data[i].y << ")" << endl;
-        cout << "Full Real output:" << endl;
-        real_out.print();
-        cout << "Full Predicted output:" << endl;
-        predicted_out.print();
-        op.print();
-        return 1;
-      }
-    }
+    // }
 
     cout << "PASSED" << endl;
   }
