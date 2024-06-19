5562531a38ba54cce85930bee5bdd615f12a1838
Federico Lolli <federico123579@gmail.com>
2024-06-19 18:19:42 +0200

    [matmul] Fix wrong format (now column-major)

--- src/tests/golden_reader.cpp
+++ src/tests/golden_reader.cpp
@@ -43,11 +43,11 @@ void Tens::print() const {
   for (int i = 0; i < 1 << rank; i++) {
     for (int j = 0; j < 1 << rank; j++) {
       if (reversed) {
-        printf("(%f + %fi) ", data[i * (1 << rank) + j].real,
-               data[i * (1 << rank) + j].imag);
-      } else {
         printf("(%f + %fi) ", data[j * (1 << rank) + i].real,
                data[j * (1 << rank) + i].imag);
+      } else {
+        printf("(%f + %fi) ", data[i * (1 << rank) + j].real,
+               data[i * (1 << rank) + j].imag);
       }
     }
     printf("\n");
