d2ebc9b92afec0e6c94fe9ba1574790ef2848164
Federico Lolli <federico123579@gmail.com>
2024-11-13 17:20:40 +0100

    [TP] Completed first version of Tensor Product Left

--- src/kernels/tensor-expansion/krnl_left_tp.cpp
+++ src/kernels/tensor-expansion/krnl_left_tp.cpp
@@ -1,75 +1,171 @@
 #include "krnl_left_tp.h"
 
+using namespace std;
+
 void krnl_left_tp(Tensor::complex_t *A, Tensor::complex_t *C, rank_t A_R) {
   // clang-format off
-#pragma HLS INTERFACE m_axi port=A bundle=gmem0 depth=16
-#pragma HLS INTERFACE m_axi port=C bundle=gmem2 depth=256
+#pragma HLS INTERFACE m_axi port=A bundle=gmem0 depth=16 // TODO change depth
+#pragma HLS INTERFACE m_axi port=C bundle=gmem2 depth=256 // TODO change depth
 #pragma HLS INTERFACE s_axilite port=A_R bundle=control
 #pragma HLS INTERFACE s_axilite port=return bundle=control
   // clang-format on
 
   using namespace Tensor;
+  using namespace Tensor::Product::Left;
 
-  complex_t a;
-  size_t read = 0;
-  hls::stream<complex_t> A_row, C_row;
+  cache_t CACHE;
+  size_t i = 0, reading_head = 0, writing_head = 0, elements_in_row_read = 0;
+  bool first_row_cached = false, row_exhausted = false, tensor_exhausted = false;
+  hls::stream<complex_t> A_row, A_cached, C_row;
   // clang-format off
-#pragma HLS STREAM variable=A_row depth=16
-#pragma HLS STREAM variable=C_row depth=16
+#pragma HLS STREAM variable=A_row depth=STREAM_SIZE
+#pragma HLS STREAM variable=A_cached depth=STREAM_SIZE
+#pragma HLS STREAM variable=C_row depth=STREAM_SIZE
+#pragma HLS ARRAY_PARTITION variable=H complete dim=1
   // clang-format on
 
-  // load the first row of A
-  load_tensor_row(A, 0, A_row);
-  // load again the same row of A
-  read = load_tensor_row(A, 0, A_row);
+  while (!tensor_exhausted) {
+    for (i = 0; !row_exhausted; i++) {
+      fetch_elems(A, first_row_cached, A_row, reading_head, elements_in_row_read);
+      cache_write(A_row, CACHE);
+      cache_read(CACHE, A_cached, row_exhausted, tensor_exhausted);
+      compute_first(A_cached, C_row);
+      store(C_row, C, writing_head);
+    }
 
-  // compute the first line of the tensor product
-  for (size_t i = 0; i < read; i++) {
-    a = A_row.read();
+    // if a single iteration was performed, hence the first row size is less than
+    // the cache size, then the cache is valid and the reading index can be kept as is,
+    // otherwise the reading index needs to be reset to row start index (subtracting the
+    // elements read in the row)
+    if (i == 1) {
+      first_row_cached = true;
+    } else {
+      first_row_cached = false;
+      CACHE.reset_head();
+      reading_head -= elements_in_row_read;
+    }
+
+    // reset row-wise information
+    CACHE.reset_tail();
+    row_exhausted = false;
+    elements_in_row_read = 0;
+
+    for (i = 0; !row_exhausted; i++) {
+      fetch_elems(A, first_row_cached, A_row, reading_head, elements_in_row_read);
+      cache_write(A_row, CACHE);
+      cache_read(CACHE, A_cached, row_exhausted, tensor_exhausted);
+      compute_second(A_cached, C_row);
+      store(C_row, C, writing_head);
+    }
+
+    // reset row-wise information
+    first_row_cached = false;
+    row_exhausted = false;
+    elements_in_row_read = 0;
+    CACHE.reset();
+  }
+}
+
+namespace Tensor {
+namespace Product {
+namespace Left {
+
+// EXECUTION PATH OPTIONS:
+// - first pass, cache valid, ready for second pass from cache
+// - first pass, cache is too small, second pass is from the first stopped,
+//   then again from the start with two passes.
+
+void fetch_elems(complex_t *A, bool first_row_cached, hls::stream<complex_t> &A_row,
+                 size_t &reading_head, size_t &elements_in_row_read) {
+  size_t ix;
+  complex_t tmp;
+
+  for (size_t i = 0; i < CACHE_SIZE; i++) {
+    if (!first_row_cached) {
+      // read from head index and update to the next one
+      ix = reading_head++;
+      tmp = A[ix]; // read from DDR
+      // increase elements read in this row
+      elements_in_row_read++;
+
+      A_row.write(tmp);
+
+      if (LAST_IN_ROW(tmp.m))
+        break;
+    }
+  }
+}
+
+void cache_write(hls::stream<complex_t> &A_row, cache_t &cache) {
+  complex_t tmp;
+
+  for (size_t i = 0; i < CACHE_SIZE; i++) {
+    // exit prematurely if stream is consumed
+    if (A_row.empty())
+      break;
+    // write to cache from stream
+    tmp = A_row.read();
+    cache.write(tmp);
+  }
+}
+
+void cache_read(cache_t &cache, hls::stream<complex_t> &A_cached, bool &row_exhausted,
+                bool &tensor_exhausted) {
+  complex_t tmp;
+
+  for (size_t i = 0; i < CACHE_SIZE; i++) {
+    // read from cache and write to stream
+    tmp = cache.read();
+    A_cached.write(tmp);
+
+    // if last in row then break
+    if (LAST_IN_ROW(tmp.m)) {
+      row_exhausted = true;
+      if (LAST_IN_TENSOR(tmp.m))
+        tensor_exhausted = true;
+      break;
+    }
+  }
+}
+
+void compute_first(hls::stream<complex_t> &A_cached, hls::stream<complex_t> &C_row) {
+  complex_t a;
+
+  for (size_t i = 0; i < CACHE_SIZE; i++) {
+    a = A_cached.read();
     X(a.m) = X(a.m) << 1;
     Y(a.m) = Y(a.m) << 1;
     LAST_IN_TENSOR(a.m) = false;
     C_row.write(a);
-  }
-
-  // compute the second
-  for (size_t i = 0; i < read; i++) {
-    a = A_row.read();
-    X(a.m) = X(a.m) << 1 + 1;
-    Y(a.m) = Y(a.m) << 1 + 1;
-    C_row.write(a);
-  }
-
-  // Tensor::Expansion::tensor_expansion_chunked(A, B, C, A_R, B_R);
-}
-
-namespace Tensor {
-
-size_t load_tensor_row(complex_t *M, size_t index, hls::stream<complex_t> &M_stream) {
-  complex_t tmp;
-  size_t n = 0, i = index;
-
-LOAD_TENSOR_LOOP:
-  for (;; i++) {
-    // clang-format off
-#pragma HLS PIPELINE II=1
-    // clang-format on
-
-    tmp = M[index + i];
-    M_stream.write(tmp);
-    n++;
-
-    // break only when reaching the end of the tensor
-    if (LAST_IN_TENSOR(tmp.m)) {
+    if (A_cached.empty())
       break;
-    }
   }
-
-  // return the number of elements read
-  return n;
 }
 
-namespace Product {
-namespace Left {} // namespace Left
+void compute_second(hls::stream<complex_t> &A_cached, hls::stream<complex_t> &C_row) {
+  complex_t a;
+
+  for (size_t i = 0; i < CACHE_SIZE; i++) {
+    a = A_cached.read();
+    X(a.m) = (X(a.m) << 1) + 1;
+    Y(a.m) = (Y(a.m) << 1) + 1;
+    C_row.write(a);
+    if (A_cached.empty())
+      break;
+  }
+}
+
+void store(hls::stream<complex_t> &C_row, complex_t *C, size_t &writing_head) {
+  complex_t tmp;
+
+  for (size_t i = 0; i < CACHE_SIZE; i++) {
+    if (C_row.empty())
+      break;
+    tmp = C_row.read();
+    C[writing_head++] = tmp;
+  }
+}
+
+} // namespace Left
 } // namespace Product
 } // namespace Tensor
--- src/kernels/tensor-expansion/krnl_left_tp.h
+++ src/kernels/tensor-expansion/krnl_left_tp.h
@@ -27,14 +27,75 @@ namespace Tensor {
  * @param M_stream stream to write the row to
  * @return number of elements read
  */
-size_t load_tensor_row(complex_t *M, size_t index, hls::stream<complex_t> &M_stream);
+// size_t load_tensor_row(complex_t *M, size_t index, hls::stream<complex_t> &M_stream);
 
 namespace Product {
 namespace Left {
 
-// if CHUNK_SIZE is changed, the compute function must be updated accordingly (stream
-// depths, etc.)
-constexpr uint16_t CHUNK_SIZE = 8;
+constexpr uint16_t CACHE_SIZE = 8;
+constexpr uint16_t STREAM_SIZE = CACHE_SIZE;
+
+struct fetch_result {
+  fetch_result() { reset(); }
+
+  void reset() {
+    cache_validity = false;
+    is_first_sweep = true;
+    elements_in_row_read = 0;
+    current_index = 0;
+  }
+
+  bool cache_validity;
+  bool is_first_sweep;
+  size_t elements_in_row_read;
+  size_t current_index;
+};
+
+template <size_t SIZE> class CircularBuffer {
+public:
+  CircularBuffer() : head(0), tail(0) {}
+
+  void write(complex_t elem) {
+    elems[head] = elem;
+    head = (head + 1) % SIZE;
+  }
+
+  complex_t read() {
+    complex_t elem = elems[tail];
+    tail = (tail + 1) % SIZE;
+    return elem;
+  }
+
+  void reset_head() { head = 0; }
+
+  void reset_tail() { tail = 0; }
+
+  void reset() {
+    reset_head();
+    reset_tail();
+  }
+
+private:
+  complex_t elems[SIZE];
+  size_t head;
+  size_t tail;
+};
+
+using cache_t = CircularBuffer<CACHE_SIZE>;
+
+void fetch_elems(complex_t *A, bool first_row_cached, hls::stream<complex_t> &A_row,
+                 size_t &reading_head, size_t &elements_in_row_read);
+
+void cache_write(hls::stream<complex_t> &A_row, cache_t &cache);
+
+void cache_read(cache_t &cache, hls::stream<complex_t> &A_cached, bool &row_exhausted,
+                bool &tensor_exhausted);
+
+void compute_first(hls::stream<complex_t> &A_cached, hls::stream<complex_t> &C_row);
+
+void compute_second(hls::stream<complex_t> &A_cached, hls::stream<complex_t> &C_row);
+
+void store(hls::stream<complex_t> &C_row, complex_t *C, size_t &writing_head);
 
 } // namespace Left
 } // namespace Product
