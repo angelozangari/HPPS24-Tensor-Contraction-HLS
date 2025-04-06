9b5c59536cfb73f83e83d889d3838cb810b36818
Federico Lolli <federico123579@gmail.com>
2025-04-06 17:45:54 +0200

    simplified left tp (baseline version)

--- src/kernels/tensor-expansion/krnl_left_tp.cpp
+++ src/kernels/tensor-expansion/krnl_left_tp.cpp
@@ -1,11 +1,12 @@
 #include "krnl_left_tp.h"
 
 using namespace std;
+using namespace hls;
 
 void krnl_left_tp(Tensor::complex_t *A, Tensor::complex_t *C, rank_t A_R, dim_t size) {
   // clang-format off
-#pragma HLS INTERFACE m_axi port=A bundle=gmem0 depth=16
-#pragma HLS INTERFACE m_axi port=C bundle=gmem2 depth=32
+#pragma HLS INTERFACE m_axi port=A bundle=gmem0 depth=4096
+#pragma HLS INTERFACE m_axi port=C bundle=gmem0 depth=8192
 #pragma HLS INTERFACE s_axilite port=A_R bundle=control
 #pragma HLS INTERFACE s_axilite port=size bundle=control
 #pragma HLS INTERFACE s_axilite port=return bundle=control
@@ -14,62 +15,35 @@ void krnl_left_tp(Tensor::complex_t *A, Tensor::complex_t *C, rank_t A_R, dim_t 
   using namespace Tensor;
   using namespace Tensor::Product::Left;
 
-  cache_t CACHE;
-  size_t i = 0, reading_head = 0, writing_head = 0, elements_in_row_read = 0;
-  bool first_row_cached = false, row_exhausted = false, tensor_exhausted = false;
-  // clang-format off
-#pragma HLS array_partition variable=CACHE.elems type=cyclic factor=2
-  // clang-format on
+  complex_t tmp, a;
+  size_t j = 0, m = 0;
 
-  // TODO: merge the two independent loop together as a single loop
-
-TPL_MAIN_LOOP:
-  while (!tensor_exhausted) {
+TPL_FIRST_LOOP:
+  for (size_t i = 0; i < size; i++) {
     // clang-format off
-#pragma HLS PIPELINE II=1
+  #pragma HLS PIPELINE
     // clang-format on
-    // First pass: compute the first part of the tensor product
-  TPL_FIRST_PASS_LOOP:
-    for (i = 0; !row_exhausted; i++) {
-      // clang-format off
-#pragma HLS PIPELINE II=1
-      // clang-format on
-      left_tp_dataflow(A, C, CACHE, reading_head, writing_head, elements_in_row_read,
-                       first_row_cached, true, row_exhausted, tensor_exhausted, size);
-    }
-
-    // If a single iteration was performed, hence the first row size is less than
-    // the cache size, then the cache is valid and the reading index can be kept as is,
-    // otherwise the reading index needs to be reset to row start index (subtracting the
-    // elements read in the row)
-    if (i == 1) {
-      first_row_cached = true; // Cache is valid for the next pass
+    tmp = A[i];
+    a = tmp;
+    X(a.m) = X(a.m) << 1;
+    Y(a.m) = Y(a.m) << 1;
+    LAST_IN_TENSOR(a.m) = false;
+    C[m++] = a;
+    if (LAST_IN_ROW(tmp.m)) {
+    TPL_SECOND_LOOP:
+      for (size_t k = i - j; k <= i; k++) {
+        // clang-format off
+        #pragma HLS PIPELINE II=1
+        // clang-format on
+        a = A[k];
+        X(a.m) = (X(a.m) << 1) + 1;
+        Y(a.m) = (Y(a.m) << 1) + 1;
+        C[m++] = a;
+      }
+      j = 0;
     } else {
-      first_row_cached = false; // Cache is not valid, reset reading head
-      CACHE.reset_head();
-      reading_head -= elements_in_row_read;
+      j++;
     }
-
-    // Reset row-wise information for the next pass
-    CACHE.reset_tail();
-    row_exhausted = false;
-    elements_in_row_read = 0;
-
-    // Second pass: compute the second part of the tensor product
-  TPL_SECOND_PASS_LOOP:
-    for (i = 0; !row_exhausted; i++) {
-      // clang-format off
-#pragma HLS PIPELINE II=1
-      // clang-format on
-      left_tp_dataflow(A, C, CACHE, reading_head, writing_head, elements_in_row_read,
-                       first_row_cached, false, row_exhausted, tensor_exhausted, size);
-    }
-
-    // Reset row-wise information for the next iteration
-    first_row_cached = false;
-    row_exhausted = false;
-    elements_in_row_read = 0;
-    CACHE.reset();
   }
 }
 
@@ -77,159 +51,73 @@ namespace Tensor {
 namespace Product {
 namespace Left {
 
-void left_tp_dataflow(Tensor::complex_t *A, Tensor::complex_t *C, cache_t &CACHE,
-                      size_t &reading_head, size_t &writing_head,
-                      size_t &elements_in_row_read, bool first_row_cached,
-                      bool compute_fist_pass, bool &row_exhausted, bool &tensor_exhausted,
-                      dim_t size) {
-  hls::stream<complex_t> A_row, A_cached, C_row;
-  // clang-format off
-#pragma HLS STREAM variable=A_row depth=STREAM_SIZE
-#pragma HLS STREAM variable=A_cached depth=STREAM_SIZE
-#pragma HLS STREAM variable=C_row depth=STREAM_SIZE
-  // clang-format on
+// void load(complex_t *A, stream<complex_t> &A_row, size_t size) {
+//   // clang-format off
+//   #pragma HLS STREAM variable=A_row depth=STREAM_SIZE
+//   // clang-format on
 
-  // TODO how to implement this DATAFLOW pragma?
-  // CACHE is external to the dataflow pragma
-  // #pragma HLS dataflow
-  fetch_elems(A, first_row_cached, A_row, reading_head, elements_in_row_read, size);
-  cache_write(A_row, CACHE);
-  cache_read(CACHE, A_cached, row_exhausted, tensor_exhausted);
-  compute(A_cached, compute_fist_pass, C_row);
-  store(C_row, C, writing_head);
-}
+//   complex_t a;
+//   size_t j = 0;
 
-void fetch_elems(complex_t *A, bool first_row_cached, hls::stream<complex_t> &A_row,
-                 size_t &reading_head, size_t &elements_in_row_read, size_t to_read) {
-  bool end_of_row_reached = false;
-  size_t elements_read = 0;
-  complex_t tmp;
-  hls::stream<complex_t> burst_stream;
+// TPL_MAIN_LOOP:
+//   for (size_t i = 0; i < size; i++) {
+//     // clang-format off
+//     #pragma HLS PIPELINE II=1
+//     // clang-format on
+//     a = A[i];
+//     X(a.m) = X(a.m) << 1;
+//     Y(a.m) = Y(a.m) << 1;
+//     C if (LAST_IN_ROW(tmp.m)) {
+//       for (size_t k = i - j; k <= i; k++) {
+//         // clang-format off
+//         #pragma HLS PIPELINE II=1
+//         // clang-format on
+//         tmp = A[k];
+//         A_row.write(tmp);
+//       }
+//       j = 0;
+//     }
+//     j++;
+//   }
+// }
 
-LTP_FETCH_READ_BURST:
-  for (size_t i = 0; i < CACHE_SIZE; i++) {
-    // clang-format off
-#pragma HLS PIPELINE II=1
-    // clang-format on
+// void compute(stream<complex_t> &A_cached, bool first_pass, stream<complex_t> &C_row) {
+//   complex_t a;
 
-    if (!first_row_cached && reading_head < to_read) {
-      // read from head index and update to the next one
-      tmp = A[reading_head++]; // read from DDR
-      // increase elements read in this row
-      elements_in_row_read++;
+// TLP_COMPUTE_LOOP:
+//   for (size_t i = 0; i < CACHE_SIZE; i++) {
+//     // clang-format off
+// #pragma HLS PIPELINE II=1
+//     // clang-format on
+//     a = A_cached.read();
+//     if (first_pass) {
+//       X(a.m) = X(a.m) << 1;
+//       Y(a.m) = Y(a.m) << 1;
+//       LAST_IN_TENSOR(a.m) = false;
+//     } else {
+//       X(a.m) = (X(a.m) << 1) + 1;
+//       Y(a.m) = (Y(a.m) << 1) + 1;
+//     }
+//     C_row.write(a);
+//     if (A_cached.empty())
+//       break;
+//   }
+// }
 
-      // cout << "Reading tmp" << endl;
-      // cout << "Last in row: " << LAST_IN_ROW(tmp.m) << endl;
-      // cout << "Last in tensor: " << LAST_IN_TENSOR(tmp.m) << endl;
-      burst_stream.write(tmp);
-      elements_read++;
-      // A_row.write(tmp);
-    }
-  }
+// void store(stream<complex_t> &C_row, complex_t *C, size_t &writing_head) {
+//   complex_t tmp;
 
-  cout << "Reading head: " << reading_head << endl;
-
-LTP_FETCH_CHECK_BOUNDARY:
-  for (size_t i = 0; i < CACHE_SIZE; i++) {
-    // clang-format off
-#pragma HLS PIPELINE II=1
-    // clang-format on
-    if (!first_row_cached && i < elements_read) {
-      tmp = burst_stream.read();
-      if (!end_of_row_reached)
-        A_row.write(tmp);
-
-      if (LAST_IN_ROW(tmp.m))
-        end_of_row_reached = true;
-    }
-  }
-
-  cout << "End of row reached: " << end_of_row_reached << endl;
-}
-
-void cache_write(hls::stream<complex_t> &A_row, cache_t &cache) {
-  complex_t tmp;
-
-TLP_CACHE_WRITE_LOOP:
-  for (size_t i = 0; i < CACHE_SIZE; i++) {
-    // clang-format off
-#pragma HLS PIPELINE II=1
-    // clang-format on
-    // exit prematurely if stream is consumed
-    // write to cache from stream
-    if (A_row.read_nb(tmp)) {
-      cout << "Reading tmp" << endl;
-      cout << "Last in row: " << LAST_IN_ROW(tmp.m) << endl;
-      cout << "Last in tensor: " << LAST_IN_TENSOR(tmp.m) << endl;
-      cache.write(tmp);
-    }
-  }
-}
-
-void cache_read(cache_t &cache, hls::stream<complex_t> &A_cached, bool &row_exhausted,
-                bool &tensor_exhausted) {
-  complex_t tmp;
-
-TLP_CACHE_READ_LOOP:
-  for (size_t i = 0; i < CACHE_SIZE; i++) {
-    // clang-format off
-#pragma HLS PIPELINE II=1
-    // clang-format on
-    // read from cache and write to stream
-    tmp = cache.read();
-    A_cached.write(tmp);
-
-    // if last in row then break
-    if (LAST_IN_ROW(tmp.m)) {
-      cout << "Last in row" << endl;
-      row_exhausted = true;
-      if (LAST_IN_TENSOR(tmp.m)) {
-        cout << "Last in tensor" << endl;
-        tensor_exhausted = true;
-      }
-      break;
-    }
-  }
-}
-
-void compute(hls::stream<complex_t> &A_cached, bool first_pass,
-             hls::stream<complex_t> &C_row) {
-  complex_t a;
-
-TLP_COMPUTE_LOOP:
-  for (size_t i = 0; i < CACHE_SIZE; i++) {
-    // clang-format off
-#pragma HLS PIPELINE II=1
-    // clang-format on
-    a = A_cached.read();
-    if (first_pass) {
-      X(a.m) = X(a.m) << 1;
-      Y(a.m) = Y(a.m) << 1;
-      LAST_IN_TENSOR(a.m) = false;
-    } else {
-      X(a.m) = (X(a.m) << 1) + 1;
-      Y(a.m) = (Y(a.m) << 1) + 1;
-    }
-    C_row.write(a);
-    if (A_cached.empty())
-      break;
-  }
-}
-
-void store(hls::stream<complex_t> &C_row, complex_t *C, size_t &writing_head) {
-  complex_t tmp;
-
-TPL_STORE_LOOP:
-  for (size_t i = 0; i < CACHE_SIZE; i++) {
-    // clang-format off
-#pragma HLS PIPELINE II=1
-    // clang-format on
-    if (C_row.empty())
-      break;
-    tmp = C_row.read();
-    C[writing_head++] = tmp;
-  }
-}
+// TPL_STORE_LOOP:
+//   for (size_t i = 0; i < CACHE_SIZE; i++) {
+//     // clang-format off
+// #pragma HLS PIPELINE II=1
+//     // clang-format on
+//     if (C_row.empty())
+//       break;
+//     tmp = C_row.read();
+//     C[writing_head++] = tmp;
+//   }
+// }
 
 } // namespace Left
 } // namespace Product
--- src/kernels/tensor-expansion/krnl_left_tp.h
+++ src/kernels/tensor-expansion/krnl_left_tp.h
@@ -22,94 +22,53 @@ namespace Tensor {
 namespace Product {
 namespace Left {
 
-constexpr uint16_t CACHE_SIZE = 8;
-constexpr uint16_t STREAM_SIZE = CACHE_SIZE;
+// constexpr uint16_t CACHE_SIZE = 8;
+constexpr uint16_t STREAM_SIZE = 8;
 
 /**
  * @brief Circular buffer implementation for caching tensor elements.
  * @tparam SIZE Size of the circular buffer.
  */
-template <std::size_t SIZE> class CircularBuffer {
-public:
-  CircularBuffer() : head(0), tail(0) {}
+// template <std::size_t SIZE> class CircularBuffer {
+// public:
+//   CircularBuffer() : head(0), tail(0) {}
 
-  void write(complex_t elem) {
-    elems[head] = elem;
-    head = (head + 1) % SIZE;
-  }
+//   void write(complex_t elem) {
+//     elems[head] = elem;
+//     head = (head + 1) % SIZE;
+//   }
 
-  complex_t read() {
-    complex_t elem = elems[tail];
-    tail = (tail + 1) % SIZE;
-    return elem;
-  }
+//   complex_t read() {
+//     complex_t elem = elems[tail];
+//     tail = (tail + 1) % SIZE;
+//     return elem;
+//   }
 
-  void reset_head() { head = 0; }
+//   void reset_head() { head = 0; }
 
-  void reset_tail() { tail = 0; }
+//   void reset_tail() { tail = 0; }
 
-  void reset() {
-    reset_head();
-    reset_tail();
-  }
+//   void reset() {
+//     reset_head();
+//     reset_tail();
+//   }
 
-  complex_t elems[SIZE];
+//   complex_t elems[SIZE];
 
-private:
-  std::size_t head;
-  std::size_t tail;
-};
+// private:
+//   std::size_t head;
+//   std::size_t tail;
+// };
 
-using cache_t = CircularBuffer<CACHE_SIZE>;
-
-/**
- * @brief Dataflow for the tensor product of a tensor A and an Identity gate (of rank 1).
- * @param A Pointer to the tensor in DDR.
- * @param C Pointer to the output tensor in DDR.
- * @param CACHE Circular buffer cache to store the elements.
- * @param reading_head Index to start reading from.
- * @param writing_head Index to start writing to.
- * @param elements_in_row_read Number of elements read in the current row.
- * @param first_row_cached Flag indicating if the first row is cached.
- * @param compute_fist_pass Flag indicating if the first pass should be computed.
- * @param row_exhausted Flag indicating if the current row is exhausted.
- * @param tensor_exhausted Flag indicating if the entire tensor is exhausted.
- */
-void left_tp_dataflow(Tensor::complex_t *A, Tensor::complex_t *C, cache_t &CACHE,
-                      std::size_t &reading_head, std::size_t &writing_head,
-                      std::size_t &elements_in_row_read, bool first_row_cached,
-                      bool compute_fist_pass, bool &row_exhausted, bool &tensor_exhausted,
-                      dim_t size);
+// using cache_t = CircularBuffer<CACHE_SIZE>;
 
 /**
  * @brief Fetch elements from tensor A and write them to a stream.
  * @param A Pointer to the tensor in DDR.
- * @param first_row_cached Flag indicating if the first row is cached.
  * @param A_row Stream to write the fetched elements.
- * @param reading_head Index to start reading from.
- * @param elements_in_row_read Number of elements read in the current row.
- * @param to_read Number of elements to read.
+ * @param size Size of the tensor.
  */
-void fetch_elems(complex_t *A, bool first_row_cached, hls::stream<complex_t> &A_row,
-                 std::size_t &reading_head, std::size_t &elements_in_row_read,
-                 size_t to_read);
-
-/**
- * @brief Write elements from a stream to the cache.
- * @param A_row Stream containing the elements to be cached.
- * @param cache Circular buffer cache to write the elements.
- */
-void cache_write(hls::stream<complex_t> &A_row, cache_t &cache);
-
-/**
- * @brief Read elements from the cache and write them to a stream.
- * @param cache Circular buffer cache to read the elements.
- * @param A_cached Stream to write the cached elements.
- * @param row_exhausted Flag indicating if the current row is exhausted.
- * @param tensor_exhausted Flag indicating if the entire tensor is exhausted.
- */
-void cache_read(cache_t &cache, hls::stream<complex_t> &A_cached, bool &row_exhausted,
-                bool &tensor_exhausted);
+// void load(complex_t *A, hls::stream<complex_t> &A_row, size_t size);
 
 /**
  * @brief Compute the pass described by the flag provided of the tensor product.
@@ -117,8 +76,8 @@ void cache_read(cache_t &cache, hls::stream<complex_t> &A_cached, bool &row_exha
  * @param first_pass True if the first pass should be computed, false if the second pass.
  * @param C_row Stream to write the computed elements.
  */
-void compute(hls::stream<complex_t> &A_cached, bool first_pass,
-             hls::stream<complex_t> &C_row);
+// void compute(hls::stream<complex_t> &A_cached, bool first_pass,
+//              hls::stream<complex_t> &C_row);
 
 /**
  * @brief Store the computed elements to the output tensor.
@@ -126,7 +85,7 @@ void compute(hls::stream<complex_t> &A_cached, bool first_pass,
  * @param C Pointer to the output tensor in DDR.
  * @param writing_head Index to start writing to.
  */
-void store(hls::stream<complex_t> &C_row, complex_t *C, std::size_t &writing_head);
+// void store(hls::stream<complex_t> &C_row, complex_t *C, std::size_t &writing_head);
 
 } // namespace Left
 } // namespace Product
